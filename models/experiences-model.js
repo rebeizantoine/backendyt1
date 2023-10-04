1ae75e3ab9cb5bb88d478236ed68ccb3c5f2d6f3
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-10-04 12:20:57 +0300

    Fixing experience section

--- models/experiences-model.js
+++ models/experiences-model.js
@@ -2,6 +2,9 @@ const mongoose=require('mongoose');
 const schema=mongoose.Schema;
 const exSchema=new schema({
     date:{type:String},
+    company:{
+        type:String
+    },
     ex_txt:{type:String}
 });
 const ExSchema=mongoose.model('Experiences',exSchema);
