6ab9de3d591278caa3478c3bc4dfd3fe38a90565
rebeizantoine <139453705+rebeizantoine@users.noreply.github.com>
2023-10-05 23:43:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const Port=5000;
 const app=express();
 app.use(express.json());
 app.use(cors());
-app.get("/", (req,res) => { res.setHeader("Access.Control.Allow.Credentials","true"); 
-res.send("API is running..");});
 con.Connectionf();
 app.listen(Port,()=>{
     console.log(`your are listening to port :${Port}`);
