2f5e8d6999ae995e765efeb8418e589afee781f1
rebeizantoine <139453705+rebeizantoine@users.noreply.github.com>
2023-10-05 21:45:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const con = require('./config/db');
-const aboutRoutes = require('./routes/about');
-const experienceRoutes = require('./routes/experience');
-const skillsRoutes = require('./routes/skill');
-const adminRoutes = require('./routes/admin');
-const projectRoutes = require('./routes/project');
-const cvRoutes = require('./routes/cv');
-const Port = 5000;
-const app = express();
-
+const express=require('express');
+const cors=require('cors');
+const con=require('./config/db');
+const aboutRoutes=require('./routes/about');
+const experienceRoutes=require('./routes/experience');
+const skillsRoutes=require('./routes/skill');
+const adminRoutes=require("./routes/admin");
+const projectRoutes=require("./routes/project");
+const cvRoutes=require("./routes/cv")
+const Port=5000;
+const app=express();
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.send("API is running..");
+app.get("/", (req,res) => { res.setHeader("Access.Control.Allow.Credentials","true"); 
+res.send("API is running..");});
+con.Connectionf();
+app.listen(Port,()=>{
+    console.log(`your are listening to port :${Port}`);
 });
-
-con.connect(); // Corrected the method name
-
-app.listen(Port, () => {
-    console.log(`You are listening to port: ${Port}`);
-});
-
-app.use('/api/about', aboutRoutes);
-app.use('/api/experiences', experienceRoutes);
-app.use('/api/skills', skillsRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/projects', projectRoutes);
-app.use('/api/cv', cvRoutes);
+app.use('/api/about',aboutRoutes);
+app.use('/api/experiences',experienceRoutes);
+app.use('/api/skills',skillsRoutes);
+app.use('/api/admin',adminRoutes);
+app.use('/api/projects',projectRoutes);
+app.use('/api/cv',cvRoutes);
