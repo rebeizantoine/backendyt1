7b62346b86ede52d271295fbe9868604bb11ffab
rebeizantoine <139453705+rebeizantoine@users.noreply.github.com>
2023-10-05 21:39:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-const express=require('express');
-const cors=require('cors');
-const con=require('./config/db');
-const aboutRoutes=require('./routes/about');
-const experienceRoutes=require('./routes/experience');
-const skillsRoutes=require('./routes/skill');
-const adminRoutes=require("./routes/admin");
-const projectRoutes=require("./routes/project");
-const cvRoutes=require("./routes/cv")
-const Port=5000;
-const app=express();
+const express = require('express');
+const cors = require('cors');
+const con = require('./config/db');
+const aboutRoutes = require('./routes/about');
+const experienceRoutes = require('./routes/experience');
+const skillsRoutes = require('./routes/skill');
+const adminRoutes = require('./routes/admin');
+const projectRoutes = require('./routes/project');
+const cvRoutes = require('./routes/cv');
+const Port = 5000;
+const app = express();
+
 app.use(express.json());
 app.use(cors());
-con.Connectionf();
-app.listen(Port,()=>{
-    console.log(`your are listening to port :${Port}`);
+
+app.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.send("API is running..");
 });
-app.use('/api/about',aboutRoutes);
-app.use('/api/experiences',experienceRoutes);
-app.use('/api/skills',skillsRoutes);
-app.use('/api/admin',adminRoutes);
-app.use('/api/projects',projectRoutes);
-app.use('/api/cv',cvRoutes);
+
+con.connect(); // Corrected the method name
+
+app.listen(Port, () => {
+    console.log(`You are listening to port: ${Port}`);
+});
+
+app.use('/api/about', aboutRoutes);
+app.use('/api/experiences', experienceRoutes);
+app.use('/api/skills', skillsRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/cv', cvRoutes);
