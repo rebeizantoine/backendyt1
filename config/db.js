f2a00bc2547f64d988d18885bf437a2dfb0e2b37
rebeizantoine <139453705+rebeizantoine@users.noreply.github.com>
2023-10-05 21:57:18 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
+
 require('dotenv').config();
-const Mongo_url=process.env.MONGO_URL;
-const mongoose=require("mongoose")
-async function  Connectionf(){
-    try{
-        await mongoose.connect(Mongo_url);
-        console.log("Connecting to database successfully");
-    }
-    catch(error){
-        console.log("An error occured");
-        console.log(error);
+
+const Mongo_url = process.env.MONGO_URL;
+const mongoose = require("mongoose");
+
+async function connectToMongoDB() {
+    try {
+        await mongoose.connect(Mongo_url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to the database successfully");
+    } catch (error) {
+        console.error("An error occurred while connecting to the database");
+        console.error(error);
     }
 }
-module.exports={Connectionf};
+
+module.exports = { connectToMongoDB };
